ddb182c57448379d37bbe7add0b0c3f07b820a54
Yamini Krishnamurthy <ykrishna@purdue.edu>
2019-04-20 16:07:16 -0400

    get rid of warnings

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import Header from './Header'
-import SignIn from './SignIn'
+//import SignIn from './SignIn'
 
 class App extends Component {
   constructor() {
