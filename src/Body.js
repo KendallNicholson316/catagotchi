f03cce7e42581a257ee8f23bf293370a6fc12b7d
Yamini Krishnamurthy <ykrishna@purdue.edu>
2019-04-20 14:46:07 -0400

    add body with cat gif

---
+++ src/Body.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import catmoving from './catmoving.gif'
+
+const Body = (props) => {
+  return (
+    <div className="Body">
+      <img src={catmoving} alt="cat moving" />
+    </div>
+  )
+}
+
+export default Body
