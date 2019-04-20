fd36bb61488fc1c3051d7f57478a487440784c35
Yamini Krishnamurthy <ykrishna@purdue.edu>
2019-04-20 16:03:09 -0400

    only display the header for now

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <SignIn />
+        <Header startDate={this.state.startDate} />
       </div>
     );
   }
